40a6d3ad29b6feff324f80e9855e5f72e6a86df1
oska1204 <oska1204@edu.campusvejle.dk>
2022-05-28 00:49:34 +0200

    adds series support

--- mongo-list.js
+++ mongo-list.js
@@ -62,47 +62,80 @@ async function createList(list, count, date, now) {
     })
     for (const obj of arr) {
         try {
-            const { title } = obj
-            const baseUrl = 'https://duckduckgo.com/?q=\\'
-            const search = encodeURIComponent(`${title} movie site:imdb.com/title`)
-            const html = await getDocument(baseUrl + search)
-            const dom = new JSDOM(html)
-            const doc = dom.window.document
-            const docTitle = doc.querySelector('head title').textContent.trim()
-            const elm = doc.querySelector('script[type="application/ld+json"]')
-            const json = JSON.parse(elm.textContent)
-            const t = doc.createElement('template')
-            t.innerHTML = json.name
-            const titleText = t.content.textContent
-            const year = docTitle.match(/(\d+)\) - IMDb/)?.[1]
-            const th = json.duration.match(/(\d+)H/)?.[1]
-            const tm = json.duration.match(/(\d+)M/)?.[1]
-            const [h, m] = [th, tm].map(e => {
-                const num = parseInt(e)
-                if (Number.isNaN(num))
-                    return 0
-                else
-                    return num
-            })
-            const mm = h * 60 + m
-            const mStr = ('0' + m).slice(-2)
-            const hhmm = `${h}:${mStr}`
-            const score = json.aggregateRating.ratingValue * 10 + '%'
-            const id = json.url.match(/tt\d{7,}/)?.toString()
-            Object.assign(obj, {
-                year,
-                mm,
-                hhmm,
-                score,
-                id,
-                old: title,
-                title: titleText,
-            })
+            await searchFn(obj);
         } catch (error) {
             console.error(error)
         }
+        await new Promise(res => setTimeout(res, 10000))
     }
+
     return arr
+}
+
+async function searchFn(obj) {
+    const { title } = obj;
+    const seriesJson = await matchEpisode(title.trim()) || {};
+    const { imdbID, seriesID } = seriesJson
+    let url;
+    let seriesTitle
+    if (imdbID) {
+        const baseUrl = 'https://www.imdb.com/title/';
+        url = baseUrl + imdbID;
+        const html = await getDocument(baseUrl + seriesID);
+        const dom = new JSDOM(html);
+        const doc = dom.window.document;
+        const elm = doc.querySelector('script[type="application/ld+json"]');
+        const json = JSON.parse(elm.textContent);
+        const t = doc.createElement('template');
+        t.innerHTML = json.name;
+        seriesTitle = t.content.textContent;
+        seriesTitle += title.match(/ s\d{2}e\d{2,}$/i).toString().toUpperCase()
+    } else {
+        const baseUrl = 'https://duckduckgo.com/?q=\\';
+        const search = encodeURIComponent(`${title} movie site:imdb.com/title`);
+        url = baseUrl + search;
+    }
+    const html = await getDocument(url);
+    const dom = new JSDOM(html);
+    const doc = dom.window.document;
+    const docTitle = doc.querySelector('head title').textContent.trim();
+    const elm = doc.querySelector('script[type="application/ld+json"]');
+    const json = JSON.parse(elm.textContent);
+    const t = doc.createElement('template');
+    t.innerHTML = json.name;
+    const titleText = t.content.textContent;
+    const year = docTitle.match(/(\d+)\) - IMDb/)?.[1];
+    const th = json.duration?.match(/(\d+)H/)?.[1];
+    const tm = json.duration?.match(/(\d+)M/)?.[1];
+    const [h, m] = [th, tm].map(e => {
+        const num = parseInt(e);
+        if (Number.isNaN(num))
+            return 0;
+
+        else
+            return num;
+    });
+    let mm = h * 60 + m;
+    const mStr = ('0' + m).slice(-2);
+    let hhmm = `${h}:${mStr}`;
+    if (mm === 0) {
+        mm = null
+        hhmm = null
+    }
+    const { ratingValue } = json.aggregateRating || {};
+    let score;
+    if (ratingValue)
+        score = ratingValue * 10 + '%';
+    const id = json.url.match(/tt\d{7,}/)?.toString();
+    Object.assign(obj, {
+        year,
+        mm,
+        hhmm,
+        score,
+        id,
+        old: title,
+        title: imdbID ? seriesTitle : titleText,
+    });
 }
 
 function getDay(now) {
@@ -146,7 +179,23 @@ async function getDocument(url) {
     return doc.documentElement.innerHTML
 }
 
+function getEpisode(search, season, episode) {
+    const str = encodeURIComponent('Obi-Wan Kenobi')
+    return fetch(`https://www.omdbapi.com/?apikey=80bf610a&t=${str}&type=series&season=${season}&episode=${episode}`)
+        .then(e => e.json())
+}
+
+function matchEpisode(str) {
+    const match = str.match(/(.+)s(\d{2})e(\d{2,})$/i)
+    if (match) {
+        const [, t, s, e] = match
+        return getEpisode(t.trim(), s, e)
+    }
+}
+
 module.exports = {
     mongoList,
-    createList
+    createList,
+    searchFn,
+    Watchlist
 }
